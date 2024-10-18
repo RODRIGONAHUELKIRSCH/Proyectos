bb338189206d82272212f3ebcf9d52aee12d991c
RODRIGONAHUELKIRSCH <mrnaaaa@gmail.com>
2024-10-17 23:16:55 -0300

    remove

--- abbruzzese-page/server/webpage.server/webpage.server/src/main/java/abbruzzese/webpage/webpage/server/webpage/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package abbruzzese.webpage.webpage.server.webpage.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import abbruzzese.webpage.webpage.server.webpage.entities.Product;
-
-public interface ProductRepository extends CrudRepository<Product,String> {
-
-}
